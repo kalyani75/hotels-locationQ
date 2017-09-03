b0b6b6df1d46ce7b57a18675235ac98283fc35f1
abalasu1 <abalasu1@in.ibm.com>
2017-09-03 15:51:36 +0530

    r1.0 latest changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 
 from location import location
 
+from gevent.wsgi import WSGIServer
 app = Flask(__name__)
 
 cors = CORS(app)
@@ -14,4 +15,5 @@ app.register_blueprint(location, url_prefix='/hotels.com')
 
 port = os.getenv('PORT', '9011')
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=int(port), debug=True)
+  http_server = WSGIServer(('0.0.0.0', int(port)), app)
+  http_server.serve_forever()
